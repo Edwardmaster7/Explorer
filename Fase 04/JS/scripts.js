17f456b6809fb63e08103164c3b03e948eb52675
Edwardmaster7 <eduardoobatista2002@hotmail.com>
2024-01-19 10:03:23 -0300

    modified:   Fase 04/JS/scripts.js

--- Fase 04/JS/scripts.js
+++ Fase 04/JS/scripts.js
@@ -1,1 +1,21 @@
-//console.log('Bem vindos ao starter')
+//  Variables and data types
+// declaration
+var name
+
+// assignment
+name = "Mike"
+
+// que tipo de dado foi armazenado na variável
+console.log(typeof name)
+
+// let age = 20
+// let isHuman = true
+
+// declaration grouping
+let age, isHuman
+
+age = 18
+isHuman = true
+
+
+console.log(name, age, isHuman)
